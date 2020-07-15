ea3c09189777bb98826270ad30d9a6219c09459b
Jameslambdastudent <63371643+Jameslambdastudent@users.noreply.github.com>
2020-07-14 22:31:03 -0400

    Having Trouble With My Checkbox

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Form from "./Form"
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center">Super-Hero Sign Up</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
